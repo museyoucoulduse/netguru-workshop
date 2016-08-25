2719f93eb62816854999abb27a80a2d34a4d2451
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-25 20:28:54 +0200

    Add teachers route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'reports#subjects'
   resources :students do
     get :subjects
   end
   get 'subjects', to: :subjects, controller: 'reports'
+  get 'teachers', to: 'teachers#show', as: :teachers
 end
