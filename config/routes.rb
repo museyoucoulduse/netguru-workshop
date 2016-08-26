1ced3eabdf4c949a4287e602e9f5677af9272a08
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-26 19:38:10 +0200

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,31 @@ Rails.application.routes.draw do
   resources :students, :teachers do
     get :subjects
   end
-  get 'subjects', action: :subjects, controller: 'reports', as: :report_subjects
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'visitors#index', as: :authenticated_root
+      resources :reports
+      resources :students, :teachers do
+          get :subjects
+      end
+
+      get 'visitors/index'
+      get 'reports/index', action: :subject, controller: 'reports', as: :report_subjects
+
+    end
+
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+      get '/students', to: redirect('/')
+      get '/teachers', to: redirect('/')
+      get '/reports', to: redirect('/')
+    end
+
+    # root 'devise/sessions#new'
+  end
+  # get 'reports/subjects', action: :subjects, controller: 'reports', as: :report_subjects
+
+
   # get 'teachers', to: 'teachers#show', as: :teachers
 end
