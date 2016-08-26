83c8ab478c10edae0f8758a8e87ea29f7ebfbabb
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-26 19:46:50 +0200

    Add full name with title to teacher decorator.

--- app/decorators/teacher_decorator.rb
+++ app/decorators/teacher_decorator.rb
@@ -4,6 +4,6 @@ class TeacherDecorator < BaseDecorator
   end
 
   def full_name_and_title
-    "#{academic_title} #{first_name} #{last_name}"
+    "#{first_name} #{last_name} [#{academic_title}]"
   end
 end
